92f82e0ca36aef42238548afe15fe4421185b1d8
Zoya Hammad <zoyahammadk@gmail.com>
2025-08-06 21:06:42 +0500

    Added fragments, abstract classes, and view binding

---
+++ RecordKeeper/app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.recordkeeper
+
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import com.example.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        supportFragmentManager.commit {
+//            add(R.id.frame_content, RunningFragment())
+//        }
+
+        // Set up BottomNavigationView with proper listener
+        binding.bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_running -> {
+                    onRunningClicked()
+                    true
+                }
+                R.id.nav_cycling -> {
+                    onCyclingClicked()
+                    true
+                }
+                else -> false
+            }
+        }
+
+//        binding.btnCycling.setOnClickListener(object : View.OnClickListener{
+//
+//            override fun onClick(v: View?) {
+//
+//            }
+//
+//        } ) --> anonymous class
+
+//        binding.btnCycling.setOnClickListener{
+//           onCyclingClicked()
+//        }
+//
+//        binding.btnRunning.setOnClickListener{
+//           onRunningClicked()
+//        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    private fun onRunningClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+    }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+//    override fun onClick(v: View?) {
+//
+//        Toast.makeText(this,"I have made the Activity a click listener",Toast.LENGTH_LONG).show()
+//    }
+
+}
